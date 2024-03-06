80569fe05f0745279e92218a6e41a045c460dc79
Sunilaparna <156899946+Sunilaparna@users.noreply.github.com>
2024-03-06 16:54:12 +0530

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,21 @@
+import { Outlet,Link} from "react-router-dom";
+ function Layout(){
+    return(
+        <>
+        <nav>
+            <ul>
+                <li> <Link to="/">Home</Link></li>
+                <li> <Link to="/Contact">About us</Link></li>
+                <li> <Link to="/Contact">Products</Link></li>
+                <li> <Link to="/Services">Services</Link></li>
+                
+                
+            </ul>
+        </nav>
+        <Outlet></Outlet>
+        
+        </>
+    )
+ }
+ export default Layout;
+
