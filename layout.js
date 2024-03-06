4f097ce1c4460803b51311435ad1a1a5cd4d76d3
Sunilaparna <156899946+Sunilaparna@users.noreply.github.com>
2024-03-06 16:55:54 +0530

    Update layout.js

--- layout.js
+++ layout.js
@@ -5,7 +5,7 @@ import { Outlet,Link} from "react-router-dom";
         <nav>
             <ul>
                 <li> <Link to="/">Home</Link></li>
-                <li> <Link to="/Contact">About us</Link></li>
+                <li> <Link to="/Contact">About</Link></li>
                 <li> <Link to="/Contact">Products</Link></li>
                 <li> <Link to="/Services">Services</Link></li>
                 
