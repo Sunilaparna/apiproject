52ac6c49cf961f238b96f397e6077513481d4030
Sunilaparna <156899946+Sunilaparna@users.noreply.github.com>
2024-03-30 10:15:14 +0530

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,21 +1,35 @@
 import { Outlet,Link} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
  function Layout(){
     return(
         <>
-        <nav>
-            <ul>
-                <li> <Link to="/">Home</Link></li>
-                <li> <Link to="/Contact">About</Link></li>
-                <li> <Link to="/Contact">Products</Link></li>
-                <li> <Link to="/Services">Services</Link></li>
-                
-                
-            </ul>
-        </nav>
-        <Outlet></Outlet>
-        
+        <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img 
+            alt=""
+            src="/"
+            width=''
+            height=''
+            />
+            BURGER STORE
+
+          </Navbar.Brand>
+
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/Aboutus">ABOUT US</Nav.Link>
+            <Nav.Link as={Link} to="/Services">SERVICES</Nav.Link>
+            <Nav.Link as={Link} to="/Menu">OUR MENU</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet></Outlet>
         </>
     )
  }
+   
  export default Layout;
 
